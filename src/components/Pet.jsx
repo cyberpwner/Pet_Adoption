872c939a71f1d17532ca2884255df6222bc930b5
cyberpwner <23579457+cyberpwner@users.noreply.github.com>
2024-05-07 00:50:58 +0200

    Removed tailwindcss

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Pet() {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Luna'),
-    React.createElement('h2', {}, 'Dog'),
-    React.createElement('h2', {}, 'Havanese'),
+    React.createElement('h1', { key: 'name' }, 'Luna'),
+    React.createElement('h2', { key: 'animal' }, 'Dog'),
+    React.createElement('h2', { key: 'breed' }, 'Havanese'),
   ]);
 }
 
