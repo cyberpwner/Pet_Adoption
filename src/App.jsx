a25c7ad4460af798df09c57b0c6f662cfd9af5d1
cyberpwner <23579457+cyberpwner@users.noreply.github.com>
2024-05-07 01:07:04 +0200

    Passing props to components the old way (React.createElement)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Pet from './components/Pet';
 
 function App() {
-  return React.createElement(
-    'div',
-    { id: '' },
-    React.createElement('h1', { style: { color: 'red' } }, 'Adopt Me!')
-  );
+  return React.createElement('div', { id: '' }, [
+    React.createElement('h1', { style: { color: 'red' } }, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mixed',
+    }),
+  ]);
 }
 
 export default App;
--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Pet() {
+function Pet({ name, animal, breed }) {
   return React.createElement('div', {}, [
-    React.createElement('h1', { key: 'name' }, 'Luna'),
-    React.createElement('h2', { key: 'animal' }, 'Dog'),
-    React.createElement('h2', { key: 'breed' }, 'Havanese'),
+    React.createElement('h1', { key: 'name' }, name),
+    React.createElement('h2', { key: 'animal' }, animal),
+    React.createElement('h2', { key: 'breed' }, breed),
   ]);
 }
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  animal: PropTypes.string.isRequired,
+  breed: PropTypes.string.isRequired,
+};
 
 export default Pet;
