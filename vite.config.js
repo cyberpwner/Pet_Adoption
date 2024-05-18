29b567cf612f4fa6fb712912c43d26839b15141d
cyberpwner <23579457+cyberpwner@users.noreply.github.com>
2024-05-18 17:17:08 +0200

    Added gh-pages so I can deploy the app

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Pet_Adoption/', // Replace <repo-name> with your repository name
+  build: {
+    outDir: 'dist',
+  },
 });
